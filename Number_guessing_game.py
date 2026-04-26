f8a327181c97028070ddfb773b0be0854f22933b
Potatopeng2013 <potatopeng2013@gmail.com>
2026-04-25 21:03:34 -0400

    First python project for practice

---
+++ Number_guessing_game.py
@@ -0,0 +1,28 @@
+import random
+
+difficulty = input("Choose a difficulty 1 to 4. 4 is the hardest: ")
+
+if difficulty == "1":
+    guess = int(input("Enter a number between 1 and 10: "))
+    number = random.randint(1, 10)
+elif difficulty == "2":
+    guess = int(input("Enter a number between 1 and 50: "))
+    number = random.randint(1, 50)
+elif difficulty == "3":
+    guess = int(input("Enter a number between 1 and 100: "))
+    number = random.randint(1, 100)
+elif difficulty == "4":
+    guess = int(input("Enter a number between 1 and 1000: "))
+    number = random.randint(1, 1000)
+else:
+    print("Not a difficulty number.")   
+
+while guess != number:
+    if guess < number:
+        print("Too low")
+    elif guess > number:
+        print("Too high")
+
+    guess = int(input("Try again: "))
+
+print("You got it")
